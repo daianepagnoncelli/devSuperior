6cc31543b703a6a31b6a7cb4e58b60ce178867e0
Daiane Pagnoncelli <d_pagnoncelli@hotmail.com>
2024-08-21 13:08:42 +0200

    Commit exercise2-add corrections

--- athletesJava/AthletesJava.java
+++ athletesJava/AthletesJava.java
@@ -1,8 +1,10 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AthletesJava {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
 
         System.out.print("How many athletes? ");
         int n = scanner.nextInt();
@@ -14,11 +16,12 @@ public class AthletesJava {
         int femaleCount = 0;
         double totalFemaleHeight = 0;
 
+
         for (int i = 1; i <= n; i++) {
             System.out.printf("Enter the data for athlete number %d:%n", i);
 
-            System.out.print("Name: ");
             scanner.nextLine();
+            System.out.print("Name: ");
             String name = scanner.nextLine();
 
             String gender;
@@ -30,31 +33,23 @@ public class AthletesJava {
                 }
             } while (!gender.equalsIgnoreCase("F") && !gender.equalsIgnoreCase("M"));
 
-            double height = -1;
-            while (height <= 0) {
+            double height;
+            do {
                 System.out.print("Height: ");
                 height = scanner.nextDouble();
                 if (height <= 0) {
-                    if (height < 0) {
-                        System.out.println("Invalid input! Height cannot be negative.");
-                    } else {
-                        System.out.println("Invalid input! Height must be a positive value.");
-                    }
+                    System.out.println("Invalid input! Please enter a positive value.");
                 }
-            }
+            } while (height <= 0);
 
-            double weight = -1;
-            while (weight <= 0) {
+            double weight;
+            do {
                 System.out.print("Weight: ");
                 weight = scanner.nextDouble();
                 if (weight <= 0) {
-                    if (weight < 0) {
-                        System.out.println("Invalid input! Weight cannot be negative.");
-                    } else {
-                        System.out.println("Invalid input! Weight must be a positive value.");
-                    }
+                    System.out.println("Invalid input! Please enter a positive value.");
                 }
-            }
+            } while (weight <= 0);
 
             totalWeight += weight;
 
@@ -76,6 +71,7 @@ public class AthletesJava {
         System.out.println("Tallest athlete: " + tallestAthleteName);
         System.out.printf("Percentage of men: %.1f %%%n", (double) maleCount / n * 100);
 
+
         if (femaleCount > 0) {
             System.out.printf("Average height of women: %.2f%n", totalFemaleHeight / femaleCount);
         } else {
@@ -85,4 +81,7 @@ public class AthletesJava {
         scanner.close();
     }
 }
+
+
+
 
